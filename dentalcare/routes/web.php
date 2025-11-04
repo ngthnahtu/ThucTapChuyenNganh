a9a46fd552ce2ca8b4e2cbce576c41919d2f071e
tu <ngthnahtu@gmail.com>
2025-11-04 16:04:53 +0700

    upload layout

---
+++ dentalcare/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('index');
+})->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/blog-single', function () {
+    return view('blog-single');
+})->name('blog-single');
+
+Route::get('/blog', function () {
+    return view('blog');
+})->name('blog');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+Route::get('/doctors', function () {
+    return view('doctors');
+})->name('doctors');
+
+Route::get('/services', function () {
+    return view('services');
+})->name('services');
+
